98b7c0845633d194ed07af7ab8fae314dfffaa64
Nindya Syaftita <1302170774.nindyasyaftita@gmail.com>
2023-11-11 15:11:28 +0700

    Menambahkan metric

--- pertemuan-09.py
+++ pertemuan-09.py
@@ -1,16 +1,26 @@
 import streamlit as st
 
+# Ini bagian heading aplikasi Streamlit
 st.title("Kuliah Praktikum Big Data")
 st.write("Nindya")
 st.write("# Heading 1")
-st.write("## Heading 2")
-st.write("### Heading 3")
+st.write("#### Heading 4")
+st.write("###### Heading 6")
 
+# Kinerja unit
+st.metric("Kinerja", 40, -1)
+st.metric("Response Time", 30, 20)
+
+# Pilihan
 pilih1=st.checkbox("Ya")
 pilih2=st.checkbox("Tidak")
 
 st.write(pilih1)
 st.write(pilih2)
+
+"""
+Ini komentar harusnya
+"""
 
 makanan=st.radio("Makanan kesukaan", ['Bakso','Nasi goreng','Mie ayam'])
 
